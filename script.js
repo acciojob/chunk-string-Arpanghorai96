cdae11697373f71f09f412481935b5ff99520e0b
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-04-25 22:13:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function stringChop(str, size) {
   // your code here
+	let arr=[];
+	if(!str)
+		return [];
+	let i=0;
+	while(i<str.length)
+		{
+			arr.push(str.slice(i,i+1));
+			i+=size;
+		}
+	return arr;
 }
 
 // Do not change the code below
